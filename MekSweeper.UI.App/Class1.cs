a6c62f7cd473c7617e65885d7ce6cf66a35308c2
SasquatchSPE <tomekmakow@gmail.com>
2020-11-28 21:29:07 -0500

    Added main view

--- MekSweeper.UI.App/Class1.cs
+++
@@ -1,8 +0,0 @@
-using System;
-
-namespace MekSweeper.UI.App
-{
-    public class Class1
-    {
-    }
-}
---
+++ MekSweeper.UI.App/MainWindowViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MekSweeper.UI.App
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+
+    }
+}
